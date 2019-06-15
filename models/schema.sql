b23885923bf4cfcf6a4eb7c8c4ea48844d3ad5e1
Andrew Kelly <andrew.kelly20@icloud.com>
2019-06-15 11:47:20 -0500

    working example

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS prancing_pony_db;
+CREATE DATABASE prancing_pony_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS prancing_pony_db;
+CREATE DATABASE prancing_pony_db;
