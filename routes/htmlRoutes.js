0d11fae408343792dc0ce03066c4fef387d85cab
Khurana <39412425+Ashish0125@users.noreply.github.com>
2019-06-19 19:34:07 -0500

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,27 @@
 var db = require("../models");
+var express = require("express");
+var path = require("path");
+var app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  //app.get("/", function(req, res) {
+    //db.Rooms.findAll({}).then(function(dbExamples) {
+      //res.render("index", {
+        //msg: "Welcome!",
+        //examples: dbExamples
+      //});
+    //});
+  //});
+
+  app.get("/", function(req,res){
+    res.sendfile((path.join(__dirname, "../public/html", "index.html")));
+  })
 
   // Load example page and pass in an example by id
   // app.get("/example/:id", function(req, res) {
