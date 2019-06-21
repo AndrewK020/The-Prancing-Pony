481e74b3a9443da6d6ed8fb26abca43b163baff0
Andrew Kelly <andrew.kelly20@icloud.com>
2019-06-20 20:29:26 -0500

    modified rooms model

--- models/room.js
+++ models/room.js
@@ -1,11 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
   
   var Room = sequelize.define("Room", {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    roomNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }
   });
-
-  Room.associate = function(models) {
   
-  };
   return Room;
 };
